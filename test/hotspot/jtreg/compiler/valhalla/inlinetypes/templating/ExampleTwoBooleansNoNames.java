854d886432458807a134fb6cfcc224868b1db632
Galder Zamarreño <galder@openjdk.org>
2025-09-10 16:41:47 +0200

    Use Record.hashCode() and initialize fields in place

--- test/hotspot/jtreg/compiler/valhalla/inlinetypes/templating/ExampleTwoBooleansNoNames.java
+++ test/hotspot/jtreg/compiler/valhalla/inlinetypes/templating/ExampleTwoBooleansNoNames.java
@@ -36,8 +36,8 @@ public class ExampleTwoBooleansNoNames
             return "%s %s".formatted(type.name(), name);
         }
 
-        String hash() {
-            return "%s.hashCode(%s)".formatted(type.boxedTypeName(), value);
+        String init() {
+            return "var %s = %s".formatted(name, value);
         }
     }
 
@@ -53,44 +53,31 @@ public class ExampleTwoBooleansNoNames
         final Map<String, Object> namedArguments = new LinkedHashMap<>();
         namedArguments.put("id", 1);
 
-        var templateHash = Template.make(() -> body(
-            """
-            int hash() {
-                return
-            """,
-            fieldArguments.stream().map(FieldArgument::hash).collect(Collectors.joining(" + ", "", " + 0;")),
-            """
-            }
-            """
-        ));
-
         var templateClass = Template.make(() -> body(
             let("id", namedArguments.get("id")),
             let("fieldDeclarations", fieldArguments.stream().map(FieldArgument::declaration).collect(Collectors.joining(", "))),
             """
-            value record Box#id(#fieldDeclarations) {
-            """,
-            templateHash.asToken(),
-            """
-            }
+            value record Box#id(#fieldDeclarations) {}
             """
         ));
 
         var templateTest = Template.make(() -> body(
             let("id", namedArguments.get("id")),
-            let("fieldValues", fieldArguments.stream().map(FieldArgument::value).map(Object::toString).collect(Collectors.joining(", "))),
+            let("fieldInits", fieldArguments.stream().map(FieldArgument::init).collect(Collectors.joining(";\n    ", "", ";"))),
+            let("fieldNames", fieldArguments.stream().map(FieldArgument::name).collect(Collectors.joining(", "))),
             """
-            private static Box#id box = new Box#id(#fieldValues);
-            private static Object GOLD#id = test#id();
+            private static int expected#id = test#id();
 
             @Test
-            public static Object test#id() {
-                return box;
+            public static int test#id() {
+                #fieldInits
+                var box = new Box#id(#fieldNames);
+                return box.hashCode();
             }
 
             @Check(test = "test#id")
-            public static void check#id(Object result) {
-                Verify.checkEQ(result, GOLD#id);
+            public static void check#id(int value) {
+                Verify.checkEQ(value, expected#id);
             }
             """
         ));
