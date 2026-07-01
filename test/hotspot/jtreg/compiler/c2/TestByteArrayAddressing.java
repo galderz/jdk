5e659636469d01280ac66b04d96526f1a4ceb89e
Galder Zamarreño <galder@openjdk.org>
2026-07-01 15:46:32 +0100

    Switch to main to see if you get 2 runs

--- test/hotspot/jtreg/compiler/c2/TestByteArrayAddressing.java
+++ test/hotspot/jtreg/compiler/c2/TestByteArrayAddressing.java
@@ -27,7 +27,7 @@
  * @summary Verify that optimal byte array addressing is in use
  * @modules java.base/jdk.internal.misc
  * @library /test/lib /
- * @run driver ${test.main.class}
+ * @run main ${test.main.class}
  * @run main/othervm -XX:+UseNewCode ${test.main.class}
  */
 
